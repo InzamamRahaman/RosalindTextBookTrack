ef39664da1eecddcbce1c90af955123a8f1f4768
IzzyRahaman <izzyrahaman@gmail.com>
2014-10-24 20:45:03 -0400

    Added solution to 1b

---
+++ 1b.fsx
@@ -0,0 +1,12 @@
+﻿
+let complement = function 
+    | 'A' -> 'T'
+    | 'C' -> 'G'
+    | 'G' -> 'C'
+    | 'T' -> 'A'
+    | _ -> failwith "Incorrect nucleotide sequence submitted"
+
+let reverseComplement xs = 
+    xs |> Seq.map (fun ch -> string(complement ch))
+       |> List.ofSeq |> List.rev |> List.toSeq |> String.concat ""
+
